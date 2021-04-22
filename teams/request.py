e807941b0f1ad3a9b18a2dc194bd2074707a08f2
daprieu <daprieu@gmail.com>
2021-04-22 09:12:56 -0500

    response coming back correctly now

--- teams/request.py
+++ teams/request.py
@@ -43,7 +43,8 @@ def get_teams(filters):
                             ts.score,
                             ts.timestamp
                         FROM Teams t
-                        LEFT OUTER JOIN TeamScore ts ON ts.teamId = t.id
+                        LEFT OUTER JOIN TeamScore ts 
+                            ON ts.teamId = t.id
                         """)
 
                         dataset = db_cursor.fetchall()
@@ -58,7 +59,7 @@ def get_teams(filters):
                             score = int(row['score']) if row['score'] is not None else 0
                             if score > 0:
                                 team_score = TeamScore(row['score_id'], row['teamId'], score, row['timeStamp'])
-                            team.scores.append(team_score.__dict__)
+                                team.scores.append(team_score.__dict__)
 
 
                     elif related_resource == "players":
